915a7f354a97664ca9b57f28588fe1d237085f6e
sand0u <zhuangsi.pak@gmail.com>
2016-05-22 16:08:17 +0800

    update

---
+++ Assets/Scripts/GameModel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameModel : MonoBehaviour {
+
+    [System.Serializable]
+    public class UFOdata
+    {
+        public int diskScale;
+        public Color diskColor;
+        public Vector3 startPos;
+        public Vector3 dirPos;
+        public float emitspeed;
+        public int emitNum;
+    }
+
+    void Awake()
+    {
+        UFOdata ufo_data = new UFOdata();
+
+    }
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
